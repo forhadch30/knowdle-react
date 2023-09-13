799e405e965075dfd2090508f6c8d5c8ba8fce34
forhad30 <forhadchariani30@gmail.com>
2023-09-13 20:34:40 +0600

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+  
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/forhadch30/knowdle-react.git
+git push -u origin main
